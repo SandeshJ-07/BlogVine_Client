624f6859d22d969f84b25396ab3ab9862b5af40d
Sandesh Jain <sandeshjain2507@gmail.com>
2023-01-18 16:34:17 +0530

    About Page -> Development Complete

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import "../src/assets/stylesheet/style.css";
 
 // Pages
 import Landing from "./Pages/Landing";
+import AboutPage from "./Pages/About";
 import Discover from "./Pages/Discover";
 import Terms from "./Pages/Terms";
 import Privacy from "./Pages/Privacy";
@@ -24,6 +25,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/discover" element={ <Discover/> }/>
+        <Route path="/about" element={<AboutPage/>}/>
         <Route path="/terms" element ={<Terms/>}/>
         <Route path="/privacy" element={<Privacy/>}/>
 
