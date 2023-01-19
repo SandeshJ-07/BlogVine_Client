df7b8bd3372a12cc49cc665641981424ef323f90
Sandesh Jain <sandeshjain2507@gmail.com>
2023-01-19 12:28:53 +0530

    Resolved Warnings

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -126,7 +126,7 @@ const Navbar = (props) => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/"
                   class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
                   <svg
